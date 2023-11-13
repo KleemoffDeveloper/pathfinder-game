0b421d628b8b66157c9a5b5f4c3faf372e62ae68
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-11-12 22:13:32 -0500

    The choices are appearing on the console

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -27,7 +27,48 @@ export default function StartGame(){
 
     function firstPrompt(){
         async function firstPromptRequest(){
-            
+            const initialRequest = await openai.chat.completions.create({
+                messages: [
+                    {
+                        role: "system",
+                        content: `We are playing an adventure game. Label the title and the plot. Give me 3 choices to progress through the story. Make each choice a single sentence.`
+                    },
+                    {
+                        role: "user",
+                        content: `Start the game.`
+                    },
+                ],
+                temperature: 0.8,
+                max_tokens: 500,
+                model: "gpt-3.5-turbo"
+            });
+
+            const response = filteredResponse(initialRequest.choices[0].message.content);
+
+            //Set our game context
+            let title = response
+                .find((element) => element.substring(0, 5) === "Title")
+                .substring(6)
+                .trim();
+
+            let plot = response
+                .find((element) => element.substring(0, 4) === "Plot")
+                .substring(5)
+                .trim();
+
+            let choices = response.filter(
+               (element) =>
+                element.includes("1.") ||
+                element.includes("2.") ||
+                element.includes("3.") ||
+                element.includes("Choices ") && element.length > 10
+            );
+
+            console.log(title, plot, choices);
         }
+        firstPromptRequest()
     }
+    useEffect(firstPrompt, [])
+
+    return;
 }
