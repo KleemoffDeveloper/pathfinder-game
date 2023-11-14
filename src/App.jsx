0ceffcbf2e386ac4a6953f752f46ad4c6af43ab8
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-11-13 20:04:05 -0500

    It's still working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState, useEffect } from "react";
 
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import HowToPlay from "./pages/HowToPlay";
+import StartGame from "./Components/StartGame";
+
 function App() {
+
+
   return (
     <div className="App">
-      <HowToPlay />
+      {/* <HowToPlay /> */}
+      {/* <GamePage /> */}
+      <StartGame />
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/Components/GamePage.jsx
+++ src/Components/GamePage.jsx
@@ -1,3 +1,33 @@
-export default function GamePage(){
+import React, { useState } from 'react';
+import StartGame from './StartGame';
+// import AnotherComponent from './AnotherComponent'; // Import the component you want to render
+
+const GamePage = () => {
+  const [showAnotherComponent, setShowAnotherComponent] = useState(false);
+
+  const handleClick = () => {
+    // Toggle the state to show/hide AnotherComponent
+    setShowAnotherComponent(!showAnotherComponent);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+
+      {showAnotherComponent && <StartGame />}
+    </div>
+  );
+};
+
+export default GamePage;
+
+
+
+
+
+
+
+
+// export default function GamePage(){
     
-}
+// }
